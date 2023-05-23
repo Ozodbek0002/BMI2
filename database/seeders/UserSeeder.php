0ed9c4d82f06798ea70327a6aedf2597690b73a3
Ozodbek <ozodbekozodov0002@gmail.com>
2023-05-23 09:15:26 +0500

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin001'),
+            'phone' => '912770919',
+            'role_id' => 1,
+            ]);
+
+        $user = User::create([
+            'name' => 'Mahalla',
+            'email' => 'mahalla@gmail.com',
+            'password' => Hash::make('mahalla001'),
+            'phone' => '912770919',
+            'role_id' => 2,
+            ]);
+
+    }
+}
